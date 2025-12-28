87bbad947a1c0ce9911e610d0de5416bb64ca5c9
Elia Kuzmanova <114006754+eliakuzmanova@users.noreply.github.com>
2025-12-28 22:33:53 +0100

    Add files via upload

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,7 +9,7 @@ import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative overflow-x-hidden w-full">
       <Navigation />
       <Hero />
       <About />
